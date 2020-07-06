8d6d47374f2cefa770795e987999e5d4414e1e8b
Artemy2807 <53526957+Artemy2807@users.noreply.github.com>
2020-07-06 23:26:54 +0700

    Update tensor.cpp

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -58,7 +58,6 @@ void Tensor::set(const std::vector<float>& data_input) {
 	this->data = malloc(sizeof(float) * data_input.size());
 	float* to_float = const_cast<float*>(data_input.data());
 	this->data = static_cast<void*>(to_float);
-	actual_shape->data()[3] = 3;
 	this->tensor = TF_NewTensor(type, actual_shape->data(), actual_shape->size(), this->data, sizeof(float) * data_input.size(), dealloator, nullptr);
 	if (tensor == nullptr) {
 		printf("[ERROR]: an error occurred allocating the Tensor memory.\n");
